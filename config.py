ebb31e8933b129ee7fd950348722f3a444d8ff0d
SupremeZ41D <54646072+supremeZ41D@users.noreply.github.com>
2019-11-13 10:38:33 -0500

    Update config.py
    - addtion of fwscript_txt and fwscript_cli.

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 from gunslinger import validation
 
-def fwscript(sshclient, path='C:/Users/omar.brito/Documents/Python_Security_Exercises/Plantilla SOC 17-10-2018.txt'):
+def fwscript_txt(sshclient, path='C:/Users/omar.brito/Documents/Python_Security_Exercises/Plantilla SOC 17-10-2018.txt'):
     txfile=open(path,'r')
     txlist=txfile.readlines()
     stdin,stdout,stderr=sshclient.exec_command(''.join(txlist))
     print('SCRIPT PUSHED!')
     txfile.close()
+
+def fwscript_cli(sshclient, script):
+    stdin,stdout,stderr=sshclient.exec_command(script)
+    print('SCRIPT PUSHED!')
 
 def commit(sshclient, paramlist):
     stdin,stdout,stderr=sshclient.exec_command('\n'.join(paramlist)+'\nend\nend\n')
